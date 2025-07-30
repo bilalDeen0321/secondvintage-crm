a43e3a77626fd3c7cd7798f545eb1048fa389e4b
Saeed <78552486+appsaeed@users.noreply.github.com>
2025-07-30 21:46:36 +0600

    Add template sources

---
+++ src/Provider.tsx
@@ -0,0 +1,21 @@
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { Toaster } from "@/components/ui/toaster";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ThemeProvider } from "./contexts/ThemeContext";
+
+const queryClient = new QueryClient();
+
+const Provider = ({ children }: { children: React.ReactNode }) => (
+    <QueryClientProvider client={queryClient}>
+        <ThemeProvider>
+            <TooltipProvider>
+                <Toaster />
+                <Sonner />
+                {children}
+            </TooltipProvider>
+        </ThemeProvider>
+    </QueryClientProvider>
+);
+
+export default Provider;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from '@vitejs/plugin-react';
+import laravel from 'laravel-vite-plugin';
 import path from "path";
-import { componentTagger } from "lovable-tagger";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -10,10 +10,12 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
   },
   plugins: [
+    laravel({
+      input: 'src/app.tsx',
+      refresh: true,
+    }),
     react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
