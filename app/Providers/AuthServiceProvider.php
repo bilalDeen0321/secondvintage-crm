f01d12e770b9fbaeca57fdd18ce23c007a4998f5
Saeed <78552486+appsaeed@users.noreply.github.com>
2025-08-02 17:14:05 +0600

    feat: register auth service provider

---
+++ app/Providers/AuthServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * The model to policy mappings for the application.
+     *
+     * @var array<class-string, class-string>
+     */
+    protected $policies = [
+        \App\Models\Watch::class => \App\Policies\WatchPolicy::class,
+        \App\Models\Batch::class => \App\Policies\BatchPolicy::class,
+        \App\Models\User::class => \App\Policies\UserPolicy::class,
+        \App\Models\WishList::class => \App\Policies\WishListPolicy::class,
+        \App\Models\Transaction::class => \App\Policies\TransactionPolicy::class,
+        // \App\Models\Setting::class => \App\Policies\SettingPolicy::class,
+        \App\Models\Log::class => \App\Policies\LogPolicy::class,
+    ];
+
+    /**
+     * Register any authentication / authorization services.
+     */
+    public function boot(): void
+    {
+        $this->registerPolicies();
+
+        // Additional gates can be defined here if needed
+    }
+}
