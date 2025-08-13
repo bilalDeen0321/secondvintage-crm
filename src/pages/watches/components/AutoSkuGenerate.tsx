02e8b8449ff9b0cdea802cc1b612e631fefeef1b
Saeed <78552486+appsaeed@users.noreply.github.com>
2025-08-13 14:52:39 +0600

    feat(watch-sku): generate sku from api to frontend response

--- src/pages/watches/components/AutoSkuGenerate.tsx
+++ src/pages/watches/components/AutoSkuGenerate.tsx
@@ -1,6 +1,7 @@
 import InputError from "@/components/InputError";
 import { Button } from "@/components/ui/button";
 import { useSkuGenerate } from "@/hooks/extarnals/useSkuGenerate";
+import { cn } from "@/lib/utils";
 import { Tag } from "lucide-react";
 import { useEffect } from "react";
 import { handlePrintSKULabel } from "../_create-actions";
@@ -28,23 +29,26 @@ export default function AutoSkuGenerate({ name, brand, onChange }: Props) {
 
     return <div>
         <label className="mb-2 block text-sm font-medium text-slate-700">
-            SKU (Auto {loading ? 'generating..' : 'generated'})
+            {sku ? 'SKU (Auto-generated)' : 'SKU (Auto-generate)'}
         </label>
-        <div className="flex items-center space-x-2">
+        <div className="flex items-center space-x-2 relative">
             <InputError message={error} className="text-sm" />
             <input
                 type="text"
                 name="sku"
-                value={sku}
+                value={loading ? 'Loading...' : sku}
                 readOnly
-                className="flex-1 cursor-not-allowed rounded-lg border border-slate-300 bg-slate-50 px-3 py-2 text-slate-600"
+                className="flex-1 cursor-not-allowed rounded-lg border border-slate-300 bg-slate-50 px-3 py-2 text-slate-600 w-full"
             />
             <Button
                 type="button"
                 variant="outline"
+                disabled={!sku}
                 size="sm"
                 onClick={() => handlePrintSKULabel(name, brand, sku)}
-                className="p-2"
+                className={cn('p-2 py-3', {
+                    'cursor-not-allowed': !sku,
+                })}
                 title="Print SKU Label"
             >
                 <Tag className="h-4 w-4" />
