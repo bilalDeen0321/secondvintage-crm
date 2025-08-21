6a7c447f17e87e374bf57047ed58ba48f6c56595
Saeed <78552486+appsaeed@users.noreply.github.com>
2025-08-21 19:12:30 +0600

    deply: add pusher to testing server

--- src/app/echo.ts
+++ src/app/echo.ts
@@ -12,8 +12,8 @@ window.Pusher = Pusher;
 
 export const echo = new Echo({
     broadcaster: 'pusher',
-    key: import.meta.env.VITE_PUSHER_APP_KEY,
-    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
+    key: import.meta.env.VITE_PUSHER_APP_KEY || 'f2699d64f893d67117f3',
+    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER || 'ap2',
     forceTLS: false,
     encrypted: true,
 });
