2621a3b03d7e211680de4bb36aba9d578e76354b
mBilal <bilalherts@gmail.com>
2025-09-27 04:27:50 +0500

    upate route accessibility

--- routes/dashboard.php
+++ routes/dashboard.php
@@ -26,8 +26,7 @@ use App\Http\Controllers\WatchController;
 use App\Http\Controllers\WishlistController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
-use Illuminate\Support\Facades\File;
-use Illuminate\Support\Facades\Response;
+
 
 
 /**
@@ -94,15 +93,4 @@ Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard'
 //fallback route
 Route::fallback(fn() => Inertia::render('NotFound'));
 
-Route::get('/storage/{folder}/{subfolder}/{filename}', function ($folder, $subfolder, $filename) {
-    $path = storage_path("app/public/{$folder}/{$subfolder}/{$filename}");
 
-    if (!File::exists($path)) {
-        abort(404);
-    }
-
-    $file = File::get($path);
-    $type = File::mimeType($path);
-
-    return Response::make($file, 200)->header("Content-Type", $type);
-});
--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,8 @@
 
 use App\Http\Controllers\Web\PreviewImageController;
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Response;
 
 /**
  * All web routes are defined in this file.
@@ -14,3 +15,15 @@ Route::name('web.')->group(function () {
 });
 
 Route::name('testing.')->group(base_path('routes/testing.php'));
+Route::get('/storage/{folder}/{subfolder}/{filename}', function ($folder, $subfolder, $filename) {
+    $path = storage_path("app/public/{$folder}/{$subfolder}/{$filename}");
+
+    if (!File::exists($path)) {
+        abort(404);
+    }
+
+    $file = File::get($path);
+    $type = File::mimeType($path);
+
+    return Response::make($file, 200)->header("Content-Type", $type);
+});
